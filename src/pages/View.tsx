7abc39da7f2ab5ab4634fe4af0a78d29eb5eceaf
Gustavo Bernardo <gustavobtflores@gmail.com>
2022-04-10 13:37:57 -0300

    Lazy load on pages other than home

--- src/pages/View.tsx
+++ src/pages/View.tsx
@@ -1,26 +1,29 @@
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import { lazy, Suspense } from "react";
 
 import Layout from "@layouts/Layout";
 
 import Home from "./Home";
-import Destination from "./Destination";
-import Crew from "./Crew";
-import Technology from "./Technology";
+const Destination = lazy(() => import("./Destination"));
+const Crew = lazy(() => import("./Crew"));
+const Technology = lazy(() => import("./Technology"));
 
 const View = () => {
   const location = useLocation();
 
   return (
     <AnimatePresence exitBeforeEnter>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="destination" element={<Destination />} />
-          <Route path="crew" element={<Crew />} />
-          <Route path="tech" element={<Technology />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<p>Loading...</p>}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="destination" element={<Destination />} />
+            <Route path="crew" element={<Crew />} />
+            <Route path="tech" element={<Technology />} />
+          </Route>
+        </Routes>
+      </Suspense>
     </AnimatePresence>
   );
 };
